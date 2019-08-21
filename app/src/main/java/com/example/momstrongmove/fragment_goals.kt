7958d4630a19abdcba8a1093028eb22b36b2124e
Thomas E Cowern <thomas.e.cowern@gmail.com>
2019-08-21 16:59:56 -0400

    finished nav bar and activities

--- app/src/main/java/com/example/momstrongmove/fragment_goals.kt
+++ app/src/main/java/com/example/momstrongmove/fragment_goals.kt
@@ -12,6 +12,7 @@ import android.view.ViewGroup
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
+private const val ARG_PARAM3 = "param3"
 
 /**
  * A simple [Fragment] subclass.
--- app/src/main/java/com/example/momstrongmove/ui/main/SectionsPagerAdapter.kt
+++ app/src/main/java/com/example/momstrongmove/ui/main/SectionsPagerAdapter.kt
@@ -8,7 +8,8 @@ import com.example.momstrongmove.R
 
 private val TAB_TITLES = arrayOf(
     R.string.tab_text_1,
-    R.string.tab_text_2
+    R.string.tab_text_2,
+    R.string.tab_text_3
 )
 
 /**
@@ -28,7 +29,7 @@ class SectionsPagerAdapter(private val context: Context, fm: FragmentManager) : 
     }
 
     override fun getCount(): Int {
-        // Show 2 total pages.
-        return 2
+        // Show 3 total pages.
+        return 3
     }
 }

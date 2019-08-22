8f07bbdf51797312ddd8bf33f6fac746a47e80aa
Thomas E Cowern <thomas.e.cowern@gmail.com>
2019-08-22 10:47:17 -0400

    added notification and follow meg activities

---
+++ app/src/main/java/com/example/momstrongmove/NotificationActivity.kt
@@ -0,0 +1,12 @@
+package com.example.momstrongmove
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class NotificationActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_notification)
+    }
+}

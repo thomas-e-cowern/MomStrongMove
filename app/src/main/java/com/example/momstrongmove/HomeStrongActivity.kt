7c7f38301d1b74d83955341bc184cf153e07634a
Thomas E Cowern <thomas.e.cowern@gmail.com>
2019-08-23 10:08:56 -0400

    updated home video cell

--- app/src/main/java/com/example/momstrongmove/HomeStrongActivity.kt
+++ app/src/main/java/com/example/momstrongmove/HomeStrongActivity.kt
@@ -7,7 +7,10 @@ import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
 import android.support.v7.widget.LinearLayoutManager
 import android.util.Log
+import com.google.gson.GsonBuilder
 import kotlinx.android.synthetic.main.activity_home_strong.*
+import okhttp3.*
+import java.io.IOException
 
 class HomeStrongActivity : AppCompatActivity() {
 
@@ -27,8 +30,42 @@ class HomeStrongActivity : AppCompatActivity() {
 
         home_recycler_view.layoutManager = LinearLayoutManager(this)
 
-        home_recycler_view.adapter = HomeStrongAdapter()
+//        home_recycler_view.adapter = HomeStrongAdapter(homeFeed)
 
+        fetchJson()
+
+    }
+
+    fun fetchJson() {
+        println("Attempting to fetch JSON")
+
+        val url = "https://api.letsbuildthatapp.com/youtube/home_feed"
+
+        val request = Request.Builder().url(url).build()
+
+        val client = OkHttpClient()
+        client.newCall(request).enqueue(object: Callback {
+
+            override fun onFailure(call: Call, e: IOException) {
+                println("Failed to execute request")
+                println(e)
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+                val body = response?.body?.string()
+                println("JSON should follow")
+                println(body)
+
+                val gson = GsonBuilder().create()
+                val homeFeed = gson.fromJson(body, HomeFeed::class.java)
+
+                runOnUiThread {
+                    home_recycler_view.adapter = HomeStrongAdapter(homeFeed)
+                }
+
+            }
+
+        })
     }
 
     private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
@@ -59,3 +96,9 @@ class HomeStrongActivity : AppCompatActivity() {
         false
     }
 }
+
+class HomeFeed (val videos: List<Video>)
+
+class Video(val id: Int, val name: String, val link: String, val imageUrl: String, val numberOfViews: Int, val channel: Channel)
+
+class Channel(val name: String, val profileImageUrl: String)
--- app/src/main/java/com/example/momstrongmove/HomeStrongAdapter.kt
+++ app/src/main/java/com/example/momstrongmove/HomeStrongAdapter.kt
@@ -6,12 +6,12 @@ import android.view.View
 import android.view.ViewGroup
 import kotlinx.android.synthetic.main.video_row.view.*
 
-class HomeStrongAdapter : RecyclerView.Adapter<CustomViewHolder>() {
+class HomeStrongAdapter(val homeFeed: HomeFeed) : RecyclerView.Adapter<CustomViewHolder>() {
 
-    val videoTitles = listOf<String>("First Video", "Second Video", "Third Video", "Fourth Video")
+//    val videoTitles = listOf<String>("First Video", "Second Video", "Third Video", "Fourth Video")
 
     override fun getItemCount(): Int {
-        return videoTitles.size
+        return homeFeed.videos.size
     }
 
     override fun onCreateViewHolder(p0: ViewGroup, p1: Int): CustomViewHolder {
@@ -21,8 +21,8 @@ class HomeStrongAdapter : RecyclerView.Adapter<CustomViewHolder>() {
     }
 
     override fun onBindViewHolder(p0: CustomViewHolder, p1: Int) {
-        val videoTitle = videoTitles.get(p1)
-        p0.view.video_row_label.text = videoTitle
+        val video = homeFeed.videos.get(p1)
+        p0.view.video_row_label.text = video.name
     }
 }
 
